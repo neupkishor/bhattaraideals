33afb215786b42bac28c6f9d8d8453e3c915520b
Prabesh Bhattarai <bhattaraideals.client@neupgroup.com>
2025-12-02 06:35:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8521047450-ce629",
+  "appId": "1:127021656380:web:4a4a57a614196f0fc75639",
+  "apiKey": "AIzaSyD9CytkT-h9m7UE4WnrldDQDpPdw2iDI20",
+  "authDomain": "studio-8521047450-ce629.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "127021656380"
+};
