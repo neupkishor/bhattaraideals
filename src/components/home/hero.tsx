6df528acafcafd019796bf19137e82dc844e01c8
Kishor Neupane <heyneupkishor@gmail.com>
2025-12-03 10:38:05 +0000

    Buy, Sell, Repair and Exchange
    break line here.
    then
    
    Your iPhone toda

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -10,7 +10,9 @@ export function Hero() {
     <section className="bg-white py-20 md:py-32">
       <div className="container mx-auto px-4 md:px-6 text-center">
         <h1 className="font-headline text-4xl md:text-6xl font-bold tracking-tighter mb-4 animate-in fade-in slide-in-from-bottom-8 duration-500">
-          Buy, Sell, Repair and Exchange your iPhone today!
+          Buy, Sell, Repair and Exchange
+          <br />
+          your iPhone today!
         </h1>
         <p className="max-w-2xl mx-auto text-muted-foreground md:text-xl mb-8 animate-in fade-in slide-in-from-bottom-10 duration-500 delay-200">
           Discover certified pre-owned Apple products at unbeatable prices,
